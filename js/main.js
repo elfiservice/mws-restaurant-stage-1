cee945d267129673d238263d5d1d4b8a425ed543
Armando Jr <elfiservice@hotmail.com>
2018-04-10 08:16:07 -0300

    refactor: modile first index page

--- js/main.js
+++ js/main.js
@@ -143,22 +143,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const divInfoContent = document.createElement('div');
+  divInfoContent.classList.add('restaurant-info');
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  divInfoContent.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  divInfoContent.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  divInfoContent.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  divInfoContent.append(more);
+
+  li.append(divInfoContent);
 
   return li
 }
